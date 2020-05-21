c2c9b3bf97931abc3958e0366ffb42f7001b8f00
pulkit4shah <pulkitshah4@gmail.com>
2020-05-21 19:49:04 +0530

    first commit

--- application/views/crud_view.php
+++ application/views/crud_view.php
@@ -10,7 +10,9 @@
            {  
                 margin:0;  
                 padding:0;  
-                background-color:#f1f1f1;  
+                background-color:#f1f1f1;
+
+                  
            }  
            .box  
            {  
